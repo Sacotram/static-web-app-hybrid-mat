42a8d785c36f21a1eef89bf4e217bc8b0e4742cb
Sacotram <mateixe@microsoft.com>
2024-05-30 12:00:27 +0100

    commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,31 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports ={
+    output:"standalone",
+}
+
+module.exports = {
+    async redirects() {
+        return [
+          {
+            source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+            destination: '<YOUR REDIRECT RULE>', 
+            permanent: false,
+          },
+        ]
+    },
+};
+
+module.exports = {
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+                    destination: '<YOUR REWRITE RULE>', 
+                }
+            ]
+        }
+    },
+};
